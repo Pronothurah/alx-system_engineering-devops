90ed66d4c52aedb3c788811a31bc73d6e85b27cf
Pronothurah <onsongojosphat28@gmail.com>
2024-02-19 15:53:40 +0300

    0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,39 +1,24 @@
 #!/usr/bin/python3
-"""script that, using this REST API, for a given employee ID,
-returns information about his/her TODO list progress."""
+"""gathers employee data from api."""
+
+import re
 import requests
 import sys
 
-
-def get_employee_todo_progress(employee_id):
-    """Returns to do progress of employee"""
-    base_url = "https://jsonplaceholder.typicode.com"
-    employee_url = f"{base_url}/users/{employee_id}"
-    todo_url = f"{base_url}/todos?userId={employee_id}"
-
-    employee_response = requests.get(employee_url)
-    employee_name = employee_response.json()["name"]
-
-    # get todos
-    todos_response = requests.get(todo_url)
-    todos = todos_response.json()
-
-    # calculate progress
-    total_tasks = len(todos)
-    completed_tasks = len([todo for todo in todos if todo["completed"]])
-    progress = f"{completed_tasks}/{total_tasks}"
-
-    # Display progress
-    print(f"Employee {employee_name} is done with tasks ({progress}):")
-    for todo in todos:
-        if todo["completed"]:
-            print(f"\t{todo['title']}")
+REST_API = "https://jsonplaceholder.typicode.com"
 
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python script.py EMPLOYEE_ID")
-        sys.exit(1)
-
-    employee_id = int(sys.argv[1])
-    get_employee_todo_progress(employee_id)
+if __name__ == '__main__':
+    if len(sys.argv) > 1 and sys.argv[1].isdigit():
+        id = int(sys.argv[1])
+        req = requests.get(f'{REST_API}/users/{id}').json()
+        task_req = requests.get(f'{REST_API}/todos').json()
+        emp_name = req.get('name')
+        tasks = [task for task in task_req if task.get('userId') == id]
+        completed_tasks = [task for task in tasks if task.get('completed')]
+        print((
+            f'Employee {emp_name} is done with '
+            f'tasks({len(completed_tasks)}/{len(tasks)}):'
+        ))
+        for task in completed_tasks:
+            print(f'\t{task.get("title")}')
