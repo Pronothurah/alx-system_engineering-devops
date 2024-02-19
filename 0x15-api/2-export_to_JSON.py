ce7d0ff230789aa175b0f61a914f1f4b086e9873
Pronothurah <onsongojosphat28@gmail.com>
2024-02-19 23:51:09 +0300

    final

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,57 +1,30 @@
 #!/usr/bin/python3
-'''A script that gathers data from an API and exports it to a JSON file.
-'''
+""" Python to get data from an API and convert to Json"""
+import csv
 import json
-import re
 import requests
 import sys
 
 
-API_URL = 'https://jsonplaceholder.typicode.com'
-'''The API's URL.'''
-
-
-def fetch_user_data(user_id):
-    '''Fetch user data from the API.'''
-    user_res = requests.get(f'{API_URL}/users/{user_id}')
-    if user_res.status_code != 200:
-        print(f'Failed to fetch user data for user ID {user_id}.')
-        return None
-    return user_res.json()
-
-
-def fetch_todos(user_id):
-    '''Fetch todos for a given user ID from the API.'''
-    todos_res = requests.get(f'{API_URL}/todos?userId={user_id}')
-    if todos_res.status_code != 200:
-        print(f'Failed to fetch todos for user ID {user_id}.')
-        return None
-    return todos_res.json()
-
-
-def export_to_json(user_id, user_data, todos):
-    '''Export user data and todos to a JSON file.'''
-    user_name = user_data.get('username')
-    user_tasks = [{'task': todo.get('title'),
-                   'completed': todo.get('completed'),
-                   'username': user_name} for todo in todos]
-    data = {str(user_id): user_tasks}
-    with open(f'{user_id}.json', 'w') as file:
-        json.dump(data, file)
-
-
 if __name__ == '__main__':
-    if len(sys.argv) != 2 or not re.fullmatch(r'\d+', sys.argv[1]):
-        print('Usage: python script.py <user_id>')
-        sys.exit(1)
+    USER_ID = sys.argv[1]
+    url_to_user = 'https://jsonplaceholder.typicode.com/users/' + USER_ID
+    res = requests.get(url_to_user)
+    """Documentation"""
+    USERNAME = res.json().get('username')
+    """Documentation"""
+    url_to_task = url_to_user + '/todos'
+    res = requests.get(url_to_task)
+    tasks = res.json()
 
-    user_id = int(sys.argv[1])
-    user_data = fetch_user_data(user_id)
-    if user_data is None:
-        sys.exit(1)
-
-    todos = fetch_todos(user_id)
-    if todos is None:
-        sys.exit(1)
-
-    export_to_json(user_id, user_data, todos)
+    dict_data = {USER_ID: []}
+    for task in tasks:
+        TASK_COMPLETED_STATUS = task.get('completed')
+        TASK_TITLE = task.get('title')
+        dict_data[USER_ID].append({
+                                  "task": TASK_TITLE,
+                                  "completed": TASK_COMPLETED_STATUS,
+                                  "username": USERNAME})
+    """print(dict_data)"""
+    with open('{}.json'.format(USER_ID), 'w') as f:
+        json.dump(dict_data, f)
