53647926c59e4a47225ccf11e542d262307b2d84
Pronothurah <onsongojosphat28@gmail.com>
2024-03-06 13:11:32 +0300

    fixes

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,9 +7,9 @@ def number_of_subscribers(subreddit):
     """Returns total subscribers of a sub reddit"""
     def number_of_subscribers(subreddit):
         """Returns total subscribers of a sub reddit"""
-        BASE_URL = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+        url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
         headers = {"User-Agent": "https://github.com/Pronothurah"}
-        response = requests.get(url=BASE_URL,
+        response = requests.get(url=url,
                                 headers=headers, allow_redirects=False)
         if response.status_code == 200:
             return response.json().get("data").get("subscribers")
